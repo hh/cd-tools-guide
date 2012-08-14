c135d1a7608dc1627ec77125d01640a74d4e0e99
Mandi Walls <mandi.walls@gmail.com>
2012-08-13 17:50:55 -0700

    updates to copyright, skin

--- source/conf.py
+++ source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CDToolsGuide'
-copyright = u'2012, Mandi Walls'
+copyright = u'2012, Opscode, Inc'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,7 +185,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'CDToolsGuide.tex', u'CDToolsGuide Documentation',
-   u'Mandi Walls', 'manual'),
+   u'Opscode, Inc', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,7 +215,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'cdtoolsguide', u'CDToolsGuide Documentation',
-     [u'Mandi Walls'], 1)
+     [u'Opscode, Inc'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,7 +229,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'CDToolsGuide', u'CDToolsGuide Documentation',
-   u'Mandi Walls', 'CDToolsGuide', 'One line description of project.',
+   u'Opscode, Inc', 'CDToolsGuide', 'One line description of project.',
    'Miscellaneous'),
 ]
 
